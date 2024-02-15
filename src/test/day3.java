0394f57b1cb11e92b7aca767d94e8a2006267931
Akila Mendis <akila@oneit.com.au>
2024-02-15 11:19:12 +0530

    132. Prioritizing the Testcases with TestNG

---
+++ src/test/day3.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+	
+	@Test
+	public void WebLoginCarLoan() {
+		System.out.println("weblogincar");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println("mobilelogincar");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		System.out.println("loginlogincar");
+	}
+}
