30b301379e74f3804f5299b0896fe7eecc09dd95
Akila Mendis <akila@oneit.com.au>
2024-02-15 12:41:42 +0530

    133. Include and  Exclude Mechanism to control testcases

---
+++ src/test/day4.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+	@Test
+	public void WebLoginCarLoan() {
+		System.out.println("weblogincar day4");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println("mobilelogincar day4");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		System.out.println("loginlogincar day4");
+	} 
+}
