e7835879e9287dbfae1025b6bdc7a233b4c25a7f
Akila Mendis <akila@oneit.com.au>
2024-02-15 10:32:32 +0530

    first commit

---
+++ src/test/day1.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	
+	@Test
+	private void demo() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+}
