1c3cb4d9681860805e01b191636b41582ae98c53
Akila Mendis <akila@oneit.com.au>
2024-02-15 13:46:44 +0530

    136. TestNG Annotations part - 2

--- src/test/day4.java
+++ src/test/day4.java
@@ -1,9 +1,23 @@
 package test;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day4 {
+	
+	@BeforeSuite
+	private void BeforeSuite() {
+		System.out.println("BeforeSuite day4");
+
+	}
+	
+	@AfterSuite
+	private void AfterSuite() {
+		System.out.println("AfterSuite day4");
+
+	}
 	
 	@BeforeTest
 	private void beforetest() {
